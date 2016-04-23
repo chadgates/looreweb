4957a2e55adda1745111c14bab252c2cbdf21d91
wl <wl@wassilios-lytrass-macbook-pro.local>
2016-04-23 23:57:15 +0200

    made media path available for local install. moved all missing elements int scss file from css.

--- config/urls.py
+++ config/urls.py
@@ -45,4 +45,4 @@ if settings.DEBUG:
         url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception("Permission Denied")}),
         url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception("Page not Found")}),
         url(r'^500/$', default_views.server_error),
-    ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
